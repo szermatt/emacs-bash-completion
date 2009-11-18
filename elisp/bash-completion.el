97cace228758efc37844574d0a8ad2ee2c066794
Stephane Zermatten <szermatt@gmx.net>
2009-11-18 20:42:11 +0100

    Split shopt sets; if one fails the rest can run

--- elisp/bash-completion.el
+++ elisp/bash-completion.el
@@ -820,10 +820,12 @@ is set to t."
 	    (bash-completion-send "function __bash_complete_wrapper { eval $__BASH_COMPLETE_WRAPPER; }" process)
 	    ;; attempt to turn off unexpected status messages from bash
 	    ;; if the current version of bash does not support these options,
-	    ;; the command will fail silently and be ignored.
+	    ;; the commands will fail silently and be ignored.
 	    ;; export MAIL='' is a desperate attempt to make sure the mail warning
 	    ;; is really switched off
-	    (bash-completion-send "shopt -u mailwarn; shopt -u checkjobs; export MAIL=''" process)
+	    (bash-completion-send "shopt -u checkjobs" process)
+	    (bash-completion-send "shopt -u mailwarn" process)
+	    (bash-completion-send "export MAIL=''" process)
 	    ;; some bash completion functions use quote_readline to double-quote
 	    ;; strings - which compgen understands but only in some environment.
 	    ;; disable this dreadful business to get a saner way of handling
