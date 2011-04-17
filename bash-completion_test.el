c0f7c20a6bdc2f504ec7e84f4ae569015727c182
Stephane Zermatten <szermatt@gmx.net>
2011-04-17 12:02:53 +0200

    added license and missing test dependency

--- bash-completion_test.el
+++ bash-completion_test.el
@@ -1,5 +1,23 @@
 ;;; bash-completion_test.el --- Tests bash-completion.el
 
+;; Copyright (C) 2009 Stephane Zermatten
+
+;; Author: Stephane Zermatten <szermatt@gmx.net>
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; `http://www.gnu.org/licenses/'.
+
 
 :;;; Commentary:
 ;;
@@ -12,9 +30,6 @@
 
 ;;; Code:
 (eval-when-compile
-  ;; force reload
-  (load-library "~/.emacs.d/bash-completion.el")
-
   (require 'sz-testutils)
   (require 'cl)
 
